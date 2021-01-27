e64af91cd5d501d43bd08f08f31f93b6b6c6b276
Almir Campos <almircampos@gmail.com>
2021-01-26 22:55:51 -0800

    [DRAFT] bash script to run the app; updated documentation

---
+++ 23.1/exercise/flask-blogly/run.sh
@@ -0,0 +1,8 @@
+export FLASK_APP=run
+export FLASK_ENV=development
+export BLOGLY_DATABASE_NAME=unit_23
+export BLOGLY_SCHEMA_NAME=flask_blogly_test
+export BLOGLY_INIT=True
+export BLOGLY_SEED=True
+flask run
+
